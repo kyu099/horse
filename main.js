b3826baa11f914721ca637c34ea6a42ad2049577
kyu099 <ryu_gelus1277@icloud.com>
2021-05-22 00:18:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -19,16 +19,19 @@ function division(n1, n2){
 
 changebutton.onclick = () => {
     let num1, num2, num3;
+    let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 
     num1 = division(score.value, num.value);
-    num2 = division(num1.quot, num.value);
-    num3 = division(num2.quot, num.value);
+    num2 = division(num1.quot, num.value-1);
+    num3 = division(num2.quot, num.value-2);
 
-    //console.log(score.value, num.value, num1, num2, num3);
+    console.log(score.value, num.value, num1, num2, num3);
 
-    out1.innerHTML = num1.rem + 1;
-    out2.innerHTML = num2.rem + 1;
-    out3.innerHTML = num3.rem + 1;
+    out1.innerHTML = number[num1.rem];
+    number.splice(num1.rem, 1);
+    out2.innerHTML = number[num2.rem];
+    number.splice(num2.rem, 1);
+    out3.innerHTML = number[num3.rem];
 
     if(score.value < 0 || score.value.includes(".")) {
         alert("スコアは0以上の整数を入力してください");
